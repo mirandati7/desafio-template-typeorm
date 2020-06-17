e66be80d68254f9ac441ccc5a5df53aa37c40536
MR7 Tecnologia <mr7app@gmail.com>
2020-06-16 21:30:26 -0300

    inicial

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,6 @@ import { Router } from 'express';
 import transactionsRouter from './transactions.routes';
 
 const routes = Router();
-
 routes.use('/transactions', transactionsRouter);
 
 export default routes;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,35 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+import TransactionRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import AppError from '../errors/AppError';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+  }: RequestDTO): Promise<Transaction> {
+    // const categoryRepository = getRepository(Category);
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = await transactionRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
